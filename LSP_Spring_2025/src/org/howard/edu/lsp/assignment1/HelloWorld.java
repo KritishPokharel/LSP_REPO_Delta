a4d6d2ba8d189ef54f4c1a455c022f2d72dee693
Kritish Pokharel <kritish.pokharel2001@gmail.com>
2025-01-23 17:01:02 -0500

    Second Time Commit

--- LSP_Spring_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP_Spring_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,9 +1,13 @@
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
-
+	/**
+	 * Kritish Pokharel
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
 
 	}
 
